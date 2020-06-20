e680901287b69ab05dac160ee08cb10a0b47023b
Justin Kumpe <jakumpe@kumpes.com>
2020-06-19 22:22:10 -0500

    first commit

---
+++ Meme Me/MemeTextDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  MemeTextDelegate.swift
+//  Sandbox
+//
+//  Created by Justin Kumpe on 6/19/20.
+//  Copyright © 2020 Justin Kumpe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTextDelegate: NSObject, UITextFieldDelegate {
+    
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth:  -3.0
+    ]
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM"{
+            textField.text = ""
+        }
+    }
+}
